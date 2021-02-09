fe6daa3b952924674340d39f9d001a38294bf1e3
zhangpeng <wa_wj999@163.com>
2021-02-10 00:32:12 +0800

    添加文件和pdf讲义

--- redis-02-springboot/src/test/java/com/zhang/Redis02SpringbootApplicationTests.java
+++ redis-02-springboot/src/test/java/com/zhang/Redis02SpringbootApplicationTests.java
@@ -1,13 +1,22 @@
 package com.zhang;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class Redis02SpringbootApplicationTests {
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Test
     void contextLoads() {
+
+//        System.out.println(redisTemplate);
+        redisTemplate.opsForValue().set("kaka","孙悟空sun");
+        System.out.println(redisTemplate.opsForValue().get("kaka"));
     }
 
 }
